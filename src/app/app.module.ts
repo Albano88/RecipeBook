2590b5ec40b04918f9c13ab7a3483a387e29b616
Albano88 <claudioalbano.88@gmail.com>
2022-02-05 19:27:32 +0100

    creati componenti header e recipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipesComponent } from './recipes/recipes.component';
+
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipesLisDetailComponent } from './recipes/recipes-lis-detail/recipes-lis-detail.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipesLisDetailComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/recipes/recipes-lis-detail/recipes-lis-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-lis-detail',
+  templateUrl: './recipes-lis-detail.component.html',
+  styleUrls: ['./recipes-lis-detail.component.css']
+})
+export class RecipesLisDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
