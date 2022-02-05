19dd11be3e6169eab9eb0dcf55f0abf7dccdfc6c
Albano88 <claudioalbano.88@gmail.com>
2022-02-05 20:00:11 +0100

    componenti controllati e corretti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes/recipes.component';
 
-import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
-import { RecipesLisDetailComponent } from './recipes/recipes-lis-detail/recipes-lis-detail.component';
+
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RecipesComponent,
-    RecipesListComponent,
-    RecipesLisDetailComponent,
     HeaderComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
   ],
   imports: [
     BrowserModule,
