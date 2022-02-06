361bffba7a59bbd1b8c36cae5d21a7a92ea20afc
Albano88 <claudioalbano.88@gmail.com>
2022-02-06 18:17:48 +0100

    aggunto modello di ricetta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
     ShoppingEditComponent,
     RecipeDetailComponent,
     RecipeListComponent,
+    RecipeItemComponent,
   ],
   imports: [
     BrowserModule,
