19277a0727315327c135de4f11fa867e626986af
Albano88 <claudioalbano.88@gmail.com>
2022-02-27 16:57:07 +0100

    modifiche visualiz. ricetta completa 89

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -13,7 +13,7 @@ export class RecipeListComponent implements OnInit {
     new Recipe('A Test Recipe', 'This is simply test',
     'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
 
-      new Recipe('A Test Recipe', 'This is simply test',
+      new Recipe('Another Test Recipe', 'This is simply test',
       'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272')
 
   ];
